2fe7fbb18d43bf4ec6ad499fb8403c3d22c96db0
CodeTheDev <zacharytennant999@yahoo.com>
2020-05-30 21:16:07 -0400

    Create ParticleManager.java

---
+++ src/main/java/net/CodeError/throwabletnt/util/ParticleManager.java
@@ -0,0 +1,55 @@
+package net.CodeError.throwabletnt.util;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+
+public class ParticleManager {
+
+	private static HashMap<UUID, Integer> activeParticles = new HashMap<UUID, Integer>();
+	private final UUID uuid;
+
+	public ParticleManager(UUID uuid) {
+
+		this.uuid = uuid;
+
+	}
+
+	public void setID(int id) {
+
+		activeParticles.put(uuid, id);
+
+	}
+
+	public int getID() {
+
+		return activeParticles.get(uuid);
+
+	}
+
+	public boolean hasID() {
+
+		if (activeParticles.containsKey(uuid)) {
+
+			return true;
+
+		}
+
+		return false;
+
+	}
+	
+	public void removeID() {
+		
+		activeParticles.remove(uuid);
+		
+	}
+	
+	public void endTask() {
+		
+		Bukkit.getScheduler().cancelTask(getID());
+		
+	}
+
+}
