dcc49ed8bde9d8083d7be92b188c5d0e48c26cbb
CodeTheDev <zacharytennant999@yahoo.com>
2020-06-07 12:47:09 -0400

    Accidentally used wrong type of entity for sender entity check. Oops.

--- src/main/java/net/CodeError/throwabletnt/Main.java
+++ src/main/java/net/CodeError/throwabletnt/Main.java
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Animals;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -70,7 +70,7 @@ public class Main extends JavaPlugin implements CommandExecutor {
 
 			if (args[0].equalsIgnoreCase("reload")) {
 
-				if (!(sender instanceof Player) && !(sender instanceof Animals)) {
+				if (!(sender instanceof Entity)) {
 					
 					this.reloadConfig();
 					this.getConfig();
